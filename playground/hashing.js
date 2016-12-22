c795ba0e6f66229defa3c076f0514600b52ab571
Jardim <JardimL@hnicorp.com>
2016-12-22 11:23:09 -0600

    Hashing user password 201612221123

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,27 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+var password = '123abc!';
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err,hash) => {
+//         console.log(hash);
+//     })
+// });
+var hashedPassword = '$2a$10$WjjvmSI2wuSxszDShaLTseN5YRYaSgN5TBVyUVKanG9vR.NhyBpVq';
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+})
 
 //JWT
-var data = {
-    id: 10
-};
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// var data = {
+//     id: 10
+// };
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 //CRYPTO-JS
 // var message = 'I am user number 3';
