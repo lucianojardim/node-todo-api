d5a9d9f50853f88415e6fd82825e8f096efbe6d4
lucianojardim <jardiml@hnicorp.com>
2016-12-22 14:52:16 -0600

    Added POST /users/login route 201612221452

--- server/models/user.js
+++ server/models/user.js
@@ -43,9 +43,9 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function () {
   var user = this;
   var access = 'auth';
-  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+  var token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
 
-  user.tokens.push({access, token});
+  user.tokens.push({ access, token });
 
   return user.save().then(() => {
     return token;
@@ -72,20 +72,39 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({ email }).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
 UserSchema.pre('save', function (next) {
   var user = this;
-  if(user.isModified('password')){
+  if (user.isModified('password')) {
     bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(user.password, salt, (err,hash) => {
-            user.password = hash;
-            next();
-        });
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
     });
-  }else{
+  } else {
     next();
   }
 });
 
 var User = mongoose.model('User', UserSchema);
 
-module.exports = {User}
+module.exports = { User }
